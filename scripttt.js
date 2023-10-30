7e7da72a50269dc2109af358eed780ff29486fae
Sagarshah68 <130995184+Sagarshah68@users.noreply.github.com>
2023-10-30 20:28:55 +0530

    Add files via upload

---
+++ scripttt.js
@@ -0,0 +1,6 @@
+// You can add JavaScript code for interactive features here.
+// For example, you can add code for a contact form or an image slider.
+// For simplicity, let's add a simple alert on page load.
+window.onload = function () {
+    alert("Welcome to My Portfolio!");
+};
